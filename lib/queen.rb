0c0729794c662202698006295f2a819243fd8b54
Lambert Francis <lfrancis18@punahou.edu>
2020-07-18 15:06:08 -0400

    get possible queen moves ignoring other pieces

--- lib/queen.rb
+++ lib/queen.rb
@@ -2,8 +2,28 @@
 
 class Queen < Piece
 
-	def initialize()	
-		super()
+	def initialize(color = "white")	
+		super(color)
+	end
+
+
+	def get_possible_moves()
+		i, j = @pos
+		moves = []
+		for k in -8..8
+			m1 = [i + k, j + k]
+			m2 = [i - k, j + k]
+			m3 = [i + k, j]
+			m4 = [i, j + k]
+			[m1, m2, m3, m4].each {|x| moves.push(x) if valid_move?(x)}
+		end
+		return moves
+	end
+
+	def valid_move?(move)
+		x, y = move
+		return false if x < 0 || x > 7 || y < 0 || y > 7
+		return true
 	end
 
 
