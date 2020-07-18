34dbf1eca8100d97e258b9a480eb26be866d769b
Lambert Francis <lfrancis18@punahou.edu>
2020-07-18 15:15:47 -0400

    Pawn movement given no other pieces

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -8,4 +8,15 @@ class Pawn < Piece
 	end
 
 
+	def get_possible_moves()
+		i, j = @pos
+		moves = []
+		moves.push([i + 1, j]) if @color == "white"
+		moves.push([i -1, j]) if @color == "white"
+		moves.push([i + 2, j]) if i == 1 && @color == "white"
+		moves.push([i -2, j]) if i == 6 && @color == "black"
+		moves = moves.select {|move| move.valid_move?()}
+
+	end
+
 end
