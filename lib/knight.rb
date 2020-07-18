e9ae5424e4c8b106cb0fa8169776eda4206e5a93
Lambert Francis <lfrancis18@punahou.edu>
2020-07-18 14:55:42 -0400

    get possible knight moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -7,4 +7,24 @@ class Knight < Piece
 	end
 
 
+
+	def get_possible_moves()
+		i, j = @pos
+		dx = [1, 1, 2, 2, -1, -1, -2, -2]
+		dy = [2, -2, 1, -1, 2, -2, 1, -1]
+		moves = []
+		for k in 0..7
+			move = [i + dx[k], j + dy[k]]
+			moves.push(move) if valid_move?(move)
+		end
+		return moves
+	end
+
+
+	def valid_move?(move)
+		x, y = move
+		return false if x < 0 || x > 7 || y < 0 || y > 7
+	end
+
+
 end
